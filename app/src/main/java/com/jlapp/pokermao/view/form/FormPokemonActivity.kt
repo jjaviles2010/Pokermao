916fcec3301dadb6fb92837757f2f3a6406ba61a
Jose Javier Aviles Gomez <jjaviles2010@gmail.com>
2019-09-10 22:44:33 -0300

    Implementing dependency injection for baseUrl.

--- app/src/main/java/com/jlapp/pokermao/view/form/FormPokemonActivity.kt
+++ app/src/main/java/com/jlapp/pokermao/view/form/FormPokemonActivity.kt
@@ -2,18 +2,20 @@ package com.jlapp.pokermao.view.form
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.SeekBar
+import android.widget.TextView
 import com.jlapp.pokermao.R
 import com.jlapp.pokermao.model.Pokemon
-import com.jlapp.pokermao.utils.URLProvider
 import com.squareup.picasso.Picasso
 import kotlinx.android.synthetic.main.activity_form_pokemon.*
 import org.koin.android.ext.android.inject
 import org.koin.android.viewmodel.ext.android.viewModel
+import org.koin.core.qualifier.named
 
 class FormPokemonActivity : AppCompatActivity() {
 
     val formPokemonViewModel: FormPokemonViewModel by viewModel()
-    val baseUrl : String by inject()
+    val baseUrl : String by inject(named("baseURL"))
     val picasso: Picasso by inject()
 
     lateinit var pokemon : Pokemon
@@ -28,7 +30,31 @@ class FormPokemonActivity : AppCompatActivity() {
     private fun setValues() {
         pokemon = intent.getParcelableExtra("POKEMON")
         tvPokemonNameForm.text = pokemon.name
-        picasso.load("$baseUrl${pokemon.imageURL}")
+        picasso.load("$baseUrl${pokemon.imageURL}").into(ivPokemonForm)
 
+        /*sbAttack.progress = pokemon.attack
+        sbDefense.progress = pokemon.defense
+        sbPS.progress = pokemon.ps
+        sbVelocity.progress = pokemon.velocity*/
+
+        setSeekbar(sbAttack, tvAttackValue, pokemon.attack)
+        setSeekbar(sbDefense, tvDefenseValue, pokemon.defense)
+        setSeekbar(sbPS, tvPSValue, pokemon.ps)
+        setSeekbar(sbVelocity, tvVelocityValue, pokemon.velocity)
+    }
+
+    private fun setSeekbar(seekBar: SeekBar, textView: TextView, value: Int) {
+        seekBar.progress = value
+        textView.text = value.toString()
+
+        seekBar.setOnSeekBarChangeListener(object : SeekBar.OnSeekBarChangeListener {
+            override fun onProgressChanged(seekBar: SeekBar?, progress: Int, fromUser: Boolean) {
+                textView.text = progress.toString()
+            }
+
+            override fun onStartTrackingTouch(seekBar: SeekBar?) {}
+
+            override fun onStopTrackingTouch(seekBar: SeekBar?) {}
+        })
     }
 }
